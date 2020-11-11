e52bc707226c68d58d1cf384acfd1795e50b14cd
Adam Vorkel <adamvorkel@gmail.com>
2020-11-11 15:37:02 +0200

    Updates

---
+++ precache-manifest.c988a514f99952f85557d64776b0da9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e1b930a88bd9f25f8dc7e6055b3faff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5f8fa582dd9fc3f743c",
+    "url": "/static/css/main.5a3fac4f.chunk.css"
+  },
+  {
+    "revision": "b95c47668de6a1f6d01e",
+    "url": "/static/js/2.542f69dc.chunk.js"
+  },
+  {
+    "revision": "b57b63a64cfa5e0ce8f68ca3636644c3",
+    "url": "/static/js/2.542f69dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5f8fa582dd9fc3f743c",
+    "url": "/static/js/main.7a52e86c.chunk.js"
+  },
+  {
+    "revision": "d9929fa4abf0be795e20",
+    "url": "/static/js/runtime-main.922c9cd7.js"
+  }
+]);
